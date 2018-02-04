8a78ed0d54d330e047553a5ac1e13e864d8ddce6
Nick Juntilla <worldnick@gmail.com>
2018-02-03 22:06:18 -0800

    cleaning up

--- migrations/2_deploy_airdrop.js
+++ migrations/2_deploy_airdrop.js
@@ -4,8 +4,8 @@ const airdrop = artifacts.require('./airdrop.sol');
 const fs = require('fs');
 
 module.exports = (deployer) => {
-  const tokenAddress = '0xd6e49800dECb64C0e195F791348C1e87a5864FD7';
-  const addresses = ["0xd6e49800dECb64C0e195F791348C1e87a5864FD7","0xd6e49800dECb64C0e195F791348C1e87a5864FD7"];//0xd6e49800dECb64C0e195F791348C1e87a5864FD7 fs.readFileSync('./conf/addresses.txt');
+  const tokenAddress = "0xd6e49800dECb64C0e195F791348C1e87a5864FD7";
+  const addresses = ["",""];//0xd6e49800dECb64C0e195F791348C1e87a5864FD7 fs.readFileSync('./conf/addresses.txt');
   const values = [234,234];//fs.readFileSync('./conf/values.txt');
 
   return deployer.deploy(airdrop)
